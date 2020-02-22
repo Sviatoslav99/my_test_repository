026e00fa6d3d982542ed673013b0d959dc07ec0a
Sviatoslav <svyatik007@gmail.com>
2020-02-22 11:44:33 +0200

    First commit

--- main.py
+++ main.py
@@ -7,5 +7,13 @@ def hello():
     name = request.args.get("name", "World")
     return f'Sviatoslav Buzian TS-72'
 
+@app.route('/first')
+def first():
+    num_0 = None
+    num_1 = 124
+    num_2 = 123
+    name = request.args.get("name", "World")
+    return f'{num_0}, {num_1}, {num_2}'
+
 if __name__ == '__main__':
     app.run('0.0.0.0')
